60f3b9d5e8aba5733335a01cb365c145515ecb40
Hucky Kim <hawk.kim1974@gmail.com>
2021-01-19 18:18:53 +0900

    working on svg icons

--- web-front/src/components/HelloWorld.vue
+++ web-front/src/components/HelloWorld.vue
@@ -1,58 +1,93 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Fan
+        </h2>
+
+        <v-row justify="center">
+          <FanIcon width="200px" height="200px" iconColor="red" />
+          <FanIcon width="200px" height="200px" iconColor="blue" />
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Plug
+        </h2>
+
+        <v-row justify="center">
+          <GlowPlugIcon width="200px" height="200px" offColor="black" onColor="red"/>
+          <GlowPlugIcon width="200px" height="200px" offColor="black" onColor="blue"/>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Fire
+        </h2>
+
+        <v-row justify="center">
+          <FireIcon visibility="visible" width="200px" height="200px" iconColor="red"/>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Pump
+        </h2>
+
+        <v-row justify="center">
+          <PumpIcon iconColor="green" width="200px" height="200px"/>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Heater
+        </h2>
+
+        <v-row justify="center">
+          <HeaterIcon iconColor="green" width="900px" height="600px"/>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import FanIcon from './FanIcon'
+  import GlowPlugIcon from './GlowPlugIcon'
+  import FireIcon from './FireIcon'
+  import PumpIcon from './PumpIcon'
+  import HeaterIcon from './HeaterIcon'
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'HelloWorld',
+    components: {
+      FanIcon,
+      GlowPlugIcon,
+      FireIcon,
+      PumpIcon,
+      HeaterIcon,
+    },
+    data: () => ({
+    }),
+  }
+</script>
