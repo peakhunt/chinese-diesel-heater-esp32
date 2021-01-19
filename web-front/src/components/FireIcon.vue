9c1a355d26bc18d4d88f9e264a50150617c02b08
Hucky Kim <hawk.kim1974@gmail.com>
2021-01-19 18:19:27 +0900

    working on svg icons

---
+++ web-front/src/components/FireIcon.vue
@@ -0,0 +1,122 @@
+<template>
+<svg
+  :width="width"
+  :height="height"
+  role="presentation"
+  @click="startFire"
+  viewBox="0 -20 120 240">
+  <defs>
+    <linearGradient id="fire-gradient-basic" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop stop-color="#ffb200" offset="0.2" />
+      <stop stop-color="#dc0000" offset="1" />
+    </linearGradient>
+    <linearGradient id="fire-gradient-red" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop stop-color="#ffb200" offset="0" />
+      <stop stop-color="#dc0000" offset="0.9" />
+    </linearGradient>
+    <linearGradient id="fire-gradient-yellow" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop stop-color="#ffb200" offset="0.3" />
+      <stop stop-color="#dc0000" offset="1" />
+    </linearGradient>
+  </defs>
+
+  <g stroke-width="0.5" stroke-opacity="0.5" ref="fire">
+    <path class="spark" fill="#ffd700" stroke="#ff0" d="M13 90c-8,-10 -6,-14 -5,-21 3,-3 0,-7 -2,-19 8,11 18,12 7,40l0 0z"></path>
+    <path class="spark" fill="#ffd700" stroke="#ff0" d="M36 45c8,-10 6,-14 5,-21 -3,-3 0,-7 2,-19 -8,11 -18,12 -7,40l0 0z"></path>
+    <path class="spark" fill="#ffd700" stroke="#ff0" d="M63 48c8,-10 6,-14 5,-21 -3,-3 0,-7 2,-19 -8,11 -18,12 -7,40l0 0z"></path>
+    <path class="spark" fill="#ffd700" stroke="#ff0" d="M83 80c-8,-10 -6,-14 -5,-21 3,-3 0,-7 -2,-19 8,11 18,12 7,40l0 0z"></path>
+    <path class="spark" fill="#ffd700" stroke="#ff0" d="M94 126c8,-10 6,-14 5,-21 -3,-3 0,-7 2,-19 -8,11 -18,12 -7,40l0 0z"></path>
+    <path class="flame" fill="url(#fire-gradient-basic)" stroke="#ff0" d="M46 50c1,13 -15,26 -13,44 0,11 -4,-17 -2,-26 -1,-8 -3,-16 -9,-21 8,42 -28,33 -5,88 -4,-2 -8,-4 -12,-7 4,13 9,21 15,28 9,9 21,13 39,16 13,4 28,2 38,-6 10,-7 15,-19 12,-34 -3,6 -7,10 -13,12 3,-17 -14,-25 -7,-42 10,-20 3,-45 0,-32 -4,12 -11,21 -20,29 22,-70 -10,-53 -11,-88 -9,12 -14,23 -12,39z"></path>
+    <path class="flame" fill="url(#fire-gradient-yellow)" stroke="#ff0" d="M51 168c-55,-58 30,-62 -11,-121 11,39 -13,64 -21,76 -5,-15 -1,-21 5,-39 -38,42 -9,63 27,84l0 0z"></path>
+    <path class="flame" fill="url(#fire-gradient-yellow)" stroke-width="0.5" stroke="#ff0" d="M55 168c-1,-14 -3,-25 15,-38 31,-22 16,-34 -3,-57 5,22 -4,40 -12,52 -5,8 -23,23 0,43z"></path>
+    <path class="flame" fill="url(#fire-gradient-red)" stroke="#ff0" d="M63 172c9,-5 11,-20 12,-37 2,-17 7,-33 34,-36 -11,9 -20,20 -16,41 -1,31 -24,29 -30,32l0 0z"></path>
+  </g>
+</svg>
+</template>
+
+<script>
+  import gsap from 'gsap'
+
+  export default {
+    name: 'FanIcon',
+    props: {
+      width: {
+        type: [Number, String],
+        default: 18
+      },
+      height: {
+        type: [Number, String],
+        default: 18
+      },
+    },
+    mounted: function () {
+      const fire = this.$refs.fire
+      const sparks = Array.from(fire.querySelectorAll('.spark'));
+
+      sparks.forEach((s) => {
+        let tl = new gsap.timeline({
+          repeat: -1,
+          paused: true,
+          onRepeat: () => {
+            tl.duration(Math.random() + 0.2)
+          }
+        })
+        .fromTo(s, {
+          duration: 0.2,
+          scale: 2,
+          y: 40,
+          transformOrigin: 'center bottom'
+        }, {
+          scale: 0,
+          y: -60
+        });
+        this.fireTls.push(tl);
+      });
+
+      const flames = Array.from(fire.querySelectorAll('.flame'));
+      flames.forEach((f, idx) => {
+        let tl = new gsap.timeline({
+          repeat: -1,
+          paused: true,
+          onRepeat: () => {
+            tl.duration(Math.random() * 0.4 + 0.2)
+          }
+        })
+        .to(f, 0.2, {
+          scaleY: 1.1 + (idx ? 0.2 : 0),
+          scaleX: idx ? 1 : 0.8,
+          transformOrigin: 'center bottom',
+          repeat: 1,
+          yoyo: true
+        });
+        this.fireTls.push(tl);
+      });
+    },
+    methods: {
+      startFire() {
+        if (this.animating == false) {
+          this.playFire()
+        } else {
+          this.pauseFire()
+        }
+      },
+      playFire() {
+        this.fireTls.forEach((tl) => {
+          tl.play();
+        });
+        this.animating = true
+      },
+      pauseFire() {
+        this.fireTls.forEach((tl) => {
+          tl.pause();
+        });
+        this.animating = false
+      },
+    },
+    data: () => ({
+      skewAnim: null,
+      animating: false,
+			fireTls: [],
+    }),
+  }
+</script>
---
+++ web-front/src/components/PumpIcon.vue
@@ -0,0 +1,64 @@
+<template>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  :width="width"
+  :height="height"
+  viewBox="0 0 88 94.963837"
+  role="presentation"
+  @click="startPump">
+
+  <g :fill="iconColor" transform="rotate(-90 44 47.4819185)">
+    <path
+     d="M 15.8,5.7 C 15.5,9.5 14,14.2 11.7,19.2 L 8.1,27 h -4 C 0.4,27 0,27.2 0,29.5 0,31.9 0.3,32 5.8,32 h 5.8 L 16.3,21.1 C 19.9,12.8 21,8.9 21,5.1 21,0.4 20.8,0 18.6,0 16.4,0 16.2,0.5 15.8,5.7 Z"/>
+    <path
+     d="M 30,17.5 V 35 h 8.5 C 45.4,35 47,34.7 47,33.5 47,32.2 49.2,32 61,32 H 75 V 46 60 H 61 C 49.2,60 47,59.8 47,58.5 47,57.3 45.4,57 38.5,57 H 30 L 29.8,73.2 29.5,89.5 H 27 c -2.2,0 -3.1,-1.4 -8.9,-14.8 L 11.6,60 H 5.8 C 0.3,60 0,60.1 0,62.5 0,64.8 0.4,65 4.3,65 h 4.2 l 5.4,12.8 c 2.9,7 6.5,13.7 7.8,14.9 3.2,3.1 7.7,3 10.8,-0.2 2.4,-2.3 2.5,-3 2.5,-15 V 65 H 57.5 80 V 46 27 H 57.5 35 V 13.5 0 H 32.5 30 Z"/>
+    <path
+     d="m 48,21.5 c 0,2.2 0.4,2.5 3.5,2.5 1.9,0 3.5,0.4 3.5,1 0,0.6 3.8,1 9.5,1 5.7,0 9.5,-0.4 9.5,-1 0,-0.6 2.3,-1 5,-1 h 5 V 45.6 67.2 L 73.5,66.5 c -5.8,-0.4 -13.9,-0.4 -18,0 -7.1,0.6 -7.5,0.8 -7.5,3.1 V 72 H 68 88 V 45.5 19 H 68 48 Z"/>
+    <path ref="cylinder"
+     d="m 19.1,34.1 c -2.9,3.2 -4.4,8.4 -3.9,13.9 0.5,5.1 4,11 6.4,11 2.5,0 2.7,-2.8 0.5,-5.6 C 18,48.2 18.4,48 37.5,48 H 55 v 5 5 H 64.5 74 V 45.5 33 H 64.5 55 V 38.5 44 H 37.5 C 18.3,44 18.4,44.1 22.1,38.1 23.9,35.1 24,34.5 22.7,33.3 21.4,32 21,32.1 19.1,34.1 Z"/>
+  </g>
+</svg>
+</template>
+
+<script>
+  import { TweenMax } from 'gsap'
+
+  export default {
+    name: 'PumpIcon',
+    props: {
+      width: {
+        type: [Number, String],
+        default: 18
+      },
+      height: {
+        type: [Number, String],
+        default: 18
+      },
+      iconColor: {
+        type: String,
+        default: 'currentColor'
+      }
+    },
+    mounted: function () {
+      this.pumpAnim = TweenMax.to(this.$refs.cylinder, 0.25, {
+        x: -7,
+        repeat: -1,
+        ease: 'none',
+        paused: true,
+        yoyo: true,
+      });
+    },
+    methods: {
+      startPump() {
+        if (this.pumpAnim.paused() == false) {
+          this.pumpAnim.pause(0)
+        } else {
+          this.pumpAnim.play()
+        }
+      },
+    },
+    data: () => ({
+      pumpAnim: null,
+    }),
+  }
+</script>
