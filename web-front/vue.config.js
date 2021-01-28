16ac3277c5bacecbe83dca4dd7ee8bb327c53b5e
Hucky Kim <hawk.kim1974@gmail.com>
2021-01-29 08:57:26 +0900

    added compressed html content

--- web-front/vue.config.js
+++ web-front/vue.config.js
@@ -2,4 +2,26 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  pluginOptions: {
+    compression:{
+      /*
+      brotli: {
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+        deleteOriginalAssets: true,
+      }, */
+		  gzip: {
+			  filename: '[path].gz[query]',
+			  algorithm: 'gzip',
+			  include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+			  minRatio: 0.8,
+			  deleteOriginalAssets: true,
+		  }
+    }
+  }
 }
