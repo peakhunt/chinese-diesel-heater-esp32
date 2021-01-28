5c73cf8cc0253e4d1d33f668d82c2310ad01cb3e
Hucky Kim <hawk.kim1974@gmail.com>
2021-01-29 08:57:12 +0900

    added gziped content support

--- main/rest_server.c
+++ main/rest_server.c
@@ -58,6 +58,7 @@ static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filepa
   return httpd_resp_set_type(req, type);
 }
 
+#if 0
 static
 esp_err_t rest_common_get_handler(httpd_req_t *req)
 {
@@ -65,6 +66,7 @@ esp_err_t rest_common_get_handler(httpd_req_t *req)
 
   rest_server_context_t *rest_context = (rest_server_context_t *)req->user_ctx;
   strlcpy(filepath, rest_context->base_path, sizeof(filepath));
+
   if (req->uri[strlen(req->uri) - 1] == '/') {
     strlcat(filepath, "/index.html", sizeof(filepath));
   } else {
@@ -103,6 +105,67 @@ esp_err_t rest_common_get_handler(httpd_req_t *req)
   /* Close file after sending complete */
   close(fd);
   ESP_LOGI(REST_TAG, "File sending complete");
+  /* Respond with an empty chunk to signal HTTP response completion */
+  httpd_resp_send_chunk(req, NULL, 0);
+  return ESP_OK;
+}
+#endif
+
+static
+esp_err_t rest_common_br_compressed_get_handler(httpd_req_t *req)
+{
+  char filepath[FILE_PATH_MAX];
+
+  rest_server_context_t *rest_context = (rest_server_context_t *)req->user_ctx;
+
+  strlcpy(filepath, rest_context->base_path, sizeof(filepath));
+
+  if (req->uri[strlen(req->uri) - 1] == '/') {
+    strlcat(filepath, "/index.html", sizeof(filepath));
+  } else {
+    strlcat(filepath, req->uri, sizeof(filepath));
+  }
+  set_content_type_from_file(req, filepath);
+
+  int fd = open(filepath, O_RDONLY, 0);
+  if (fd == -1) {
+    //
+    // append ".gz" for compressed files
+    //
+    strlcat(filepath, ".gz", sizeof(filepath));
+    fd = open(filepath, O_RDONLY, 0);
+    if (fd == -1) {
+      ESP_LOGE(REST_TAG, "Failed to open file : %s", filepath);
+      /* Respond with 500 Internal Server Error */
+      httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
+      return ESP_FAIL;
+    }
+    httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
+  }
+
+  char *chunk = rest_context->scratch;
+  ssize_t read_bytes;
+  do {
+    /* Read file in chunks into the scratch buffer */
+    read_bytes = read(fd, chunk, SCRATCH_BUFSIZE);
+    if (read_bytes == -1) {
+      ESP_LOGE(REST_TAG, "Failed to read file : %s", filepath);
+    } else if (read_bytes > 0) {
+      /* Send the buffer contents as HTTP response chunk */
+      if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
+        close(fd);
+        ESP_LOGE(REST_TAG, "File sending failed : %s", filepath);
+        /* Abort sending file */
+        httpd_resp_sendstr_chunk(req, NULL);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
+        return ESP_FAIL;
+      }
+    }
+  } while (read_bytes > 0);
+  /* Close file after sending complete */
+  close(fd);
+  ESP_LOGI(REST_TAG, "File sending complete: %s", filepath);
   /* Respond with an empty chunk to signal HTTP response completion */
   httpd_resp_send_chunk(req, NULL, 0);
   return ESP_OK;
@@ -676,7 +739,8 @@ esp_err_t start_rest_server(const char *base_path)
   httpd_uri_t common_get_uri = {
     .uri = "/*",
     .method = HTTP_GET,
-    .handler = rest_common_get_handler,
+    //.handler = rest_common_get_handler,
+    .handler = rest_common_br_compressed_get_handler,
     .user_ctx = rest_context
   };
   httpd_register_uri_handler(server, &common_get_uri);
