daaaf02b4d69c20aa59b1e6830350dd71112f7d1
Hucky Kim <hawk.kim1974@gmail.com>
2021-02-02 14:22:47 +0900

    ifdef-loader config

--- web-front/vue.config.js
+++ web-front/vue.config.js
@@ -1,26 +1,36 @@
+const opts = {
+  RUN_TARGET: (() => {
+    if(/^electron:/.test(process.argv[2])) {
+      return "electron"
+    }
+    return "web"
+  })(),
+  "ifdef-verbose" : true,
+}
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
   pluginOptions: {
     compression:{
-		  gzip: {
-			  filename: '[path].gz[query]',
-			  algorithm: 'gzip',
-			  include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-			  minRatio: 0.8,
-			  deleteOriginalAssets: true,
-		  }
+      gzip: {
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+        deleteOriginalAssets: true,
+      }
     }
   },
-	chainWebpack: config => {
+  chainWebpack: config => {
     // ifdef-loader
     config.module
-      .rule('ifdef-loader')
-      .test(/\.(js|vue)$/)
-      .use('ifdef-loader')
-        .loader('ifdef-loader')
-        .options(opts)
-        .end()
+    .rule('ifdef-loader')
+    .test(/\.(js|vue)$/)
+    .use('ifdef-loader')
+      .loader('ifdef-loader')
+      .options(opts)
+      .end()
   }
 }
